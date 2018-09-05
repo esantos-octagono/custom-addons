73ded9942d84ecdf0217ad08a839e692bff3156c
edev <esantos-octagono@octagono.com.do>
2018-09-05 13:46:47 -0400

    arreglando heads

--- ncf_manager/static/src/js/marcos.js
+++ ncf_manager/static/src/js/marcos.js
@@ -7,11 +7,7 @@ odoo.define('ncf_manager.widgets', function (require) {
 
     var Copyright = form_common.FormWidget.extend(form_common.ReinitializeWidgetMixin, {
         start: function () {
-<<<<<<< HEAD
             // this.$el.append("<a href='https://marcos.do/page/opl?' target='_blank'>&#169; Marcos Organizador de Negocios SRL / Odoo Proprietary License v1.0</a>");
-=======
-            this.$el.append("<a href='https://marcos.do/page/opl?' target='_blank'>&#169; Marcos Organizador de Negocios SRL / Odoo Proprietary License v1.0</a>");
->>>>>>> 2a2343b54b5cb74b07be221d1377c1942efa3fd6
         }
     });
 
