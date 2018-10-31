9ee7051a09d0bafb3ec0bacd228c48f9367b98de
edev <edwin2delossantos@gmail.com>
2018-10-31 16:06:33 -0400

    Datos por defecto en modulo de prestamos, y reporte de pagos

---
+++ reporte_factura/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Reporte Pago",
+    'description': "Plantilla para reporte de pagos",
+    'version': "1.0",
+    'author': "Edwin de los Santos",
+    'depends': ['base'],
+    'data': [
+        'reports/payment.xml',
+        'views/payment_external_layout.xml'
+    ],
+    'installable': True,
+}
