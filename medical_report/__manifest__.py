0cbe53ec3883f5beab80f246cf7695d123b5890b
edev <edwin2delossantos@gmail.com>
2018-12-17 10:49:07 -0400

    reporte medico antes de modificaciones

---
+++ medical_report/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Reporte Facturas medicas",
+    'description': "Plantilla para reporte de facturas medicas",
+    'version': "10.0",
+    'author': "Edwin de los Santos",
+    'depends': ['account'],
+    'data': [
+        'reports/payment.xml',
+        'views/payment_external_layout.xml'
+    ],
+    'installable': True,
+}
