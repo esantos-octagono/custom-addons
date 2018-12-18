42eb81bb60e8b39bd82a8a84e1410bf26d28d52a
edev <edwin2delossantos@gmail.com>
2018-12-18 10:19:44 -0400

    fixing draft change error

--- lasmed/models/account_invoice.py
+++ lasmed/models/account_invoice.py
@@ -51,7 +51,10 @@ class AccountInvoice(models.Model):
         elif self.state in ['paid','cancel']:
             pass
         elif self.cober > self.amount_total and len(self.payment_ids) == 0 and self.amount_total !=0:
-            raise ValidationError("La cobertura debe ser menor o igual al total de la factura, confirme el monto con el suplidor")
+            if self.anulation_type:
+                pass
+            else:
+                raise ValidationError("La cobertura debe ser menor o igual al total de la factura, confirme el monto con el suplidor")
 
     @api.onchange('amount_untaxed','discount','invoice_line_ids')
     def _onchange_amount_total(self):
